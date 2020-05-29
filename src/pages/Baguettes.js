bca17244380abd58248092c58df1cda10738dd20
kriztiank <kriztiank@gmail.com>
2020-05-29 12:15:29 +0200

    4th commit

--- src/pages/Baguettes.js
+++ src/pages/Baguettes.js
@@ -1,17 +1,18 @@
 import React from 'react'
 import Head from '../components/head';
 import Layout from '../components/layout/layout'
+import './sub_products.scss';
 
 const Baguettes = props => {
   return (
     <Layout>
       <Head title="Products" />
-      <section className="products__section1">
+      <section className="sub__products__section1">
         <button className="btn__white__block">Produkter > Baguettes</button>
       </section>
       {props.data.allProducts.edges.map(({ node }) => {
         return (
-          <section className="products__section2">
+          <section className="sub__products__section2">
             <div key={node.id} style={{ marginBottom: 1 }}>
               <h2>{node.title}</h2>
               <p>{node.teaser}</p>
